3fca0da926d7faff4d9ca048c32c56f3e7c96321
Serbanescu Alina <stanalinaionela@yahoo.ro>
2022-09-26 20:46:17 +0300

    844

---
+++ pages/contact.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const ContactPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact Page</title>
+      </Head>
+      <main>hello from next</main>
+    </>
+  );
+};
+
+// este obligatoriu sa returnam un default export
+// pentru fiecare pagina
+export default ContactPage;
