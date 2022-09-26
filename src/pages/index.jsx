c4f1bbc216b50e1cd5df46d44c4f1d85a552ae22
Serbanescu Alina <stanalinaionela@yahoo.ro>
2022-09-26 21:43:02 +0300

    942

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main className="text-3xl font-bold underline">homepage</main>
+    </>
+  );
+};
+
+export default Home;
